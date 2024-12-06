a31aa2ff982fcf0b7c671c638a68f5b7602f03d6
Rijk van Putten <rijk@rijkvp.nl>
2024-12-06 11:27:46 +0100

    Day 6, part 2 progress..

--- day6/main.zig
+++ day6/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const input = @embedFile("input");
-const GRID_SIZE: usize = 130;
+const input = @embedFile("example");
+const GRID_SIZE: usize = 10;
 
 fn readGrid() [GRID_SIZE][GRID_SIZE]u8 {
     var grid: [GRID_SIZE][GRID_SIZE]u8 = undefined;
@@ -18,7 +18,7 @@ fn readGrid() [GRID_SIZE][GRID_SIZE]u8 {
     return grid;
 }
 
-fn calculateDir(dir: usize, row: usize, col: usize) ?[2]usize {
+fn calculateDir(dir: u3, row: usize, col: usize) ?[2]usize {
     const r: isize = @intCast(row);
     const c: isize = @intCast(col);
     const coords: [2]isize = switch (dir) {
@@ -36,28 +36,17 @@ fn calculateDir(dir: usize, row: usize, col: usize) ?[2]usize {
     return .{ r2, c2 };
 }
 
-pub fn main() !void {
-    const grid = readGrid();
-    var row: usize = 0;
-    var col: usize = 0;
-    for (0..GRID_SIZE) |r| {
-        for (0..GRID_SIZE) |c| {
-            if (grid[r][c] == '^') {
-                row = r;
-                col = c;
-                break;
-            }
-        }
-    }
-
+fn countVisisted(start_row: usize, start_col: usize, obstacles: *[GRID_SIZE][GRID_SIZE]bool) u64 {
     var visited: [GRID_SIZE][GRID_SIZE]bool = undefined;
-    var dir: usize = 0;
+    var row: usize = start_row;
+    var col: usize = start_col;
+    var dir: u3 = 0;
     while (true) {
         visited[row][col] = true;
         const next = calculateDir(dir, row, col) orelse break;
         var r = next[0];
         var c = next[1];
-        if (grid[r][c] == '#') {
+        if (obstacles[r][c]) {
             // switch direction
             dir = (dir + 1) % 4;
             const next2 = calculateDir(dir, row, col) orelse break;
@@ -67,14 +56,80 @@ pub fn main() !void {
         row = r;
         col = c;
     }
-    var part1: u63 = 0;
+    var count: u64 = 0;
     for (0..GRID_SIZE) |r| {
         for (0..GRID_SIZE) |c| {
             if (visited[r][c]) {
-                part1 += 1;
+                count += 1;
             }
         }
     }
+    return count;
+}
+const one: u8 = 1;
+
+fn loops(start_row: usize, start_col: usize, obstacles: *[GRID_SIZE][GRID_SIZE]bool) bool {
+    // bitmap
+    var visited: [GRID_SIZE][GRID_SIZE]u8 = std.mem.zeroes([GRID_SIZE][GRID_SIZE]u8);
+    var row: usize = start_row;
+    var col: usize = start_col;
+    var dir: u3 = 0;
+    while (true) {
+        if ((visited[row][col] & (one << dir)) > 0) {
+            // already visited in same direction = looping
+            return true;
+        }
+        visited[row][col] |= one << dir;
+        const next = calculateDir(dir, row, col) orelse break;
+        var r = next[0];
+        var c = next[1];
+        if (obstacles[r][c]) {
+            // switch direction
+            dir = (dir + 1) % 4;
+            const next2 = calculateDir(dir, row, col) orelse break;
+            r = next2[0];
+            c = next2[1];
+        }
+        row = r;
+        col = c;
+    }
+    return false;
+}
+
+pub fn main() !void {
+    const grid = readGrid();
+    var start_row: usize = 0;
+    var start_col: usize = 0;
+    var obstacles: [GRID_SIZE][GRID_SIZE]bool = undefined;
+    for (0..GRID_SIZE) |r| {
+        for (0..GRID_SIZE) |c| {
+            if (grid[r][c] == '#') {
+                obstacles[r][c] = true;
+                continue;
+            }
+            obstacles[r][c] = false;
+            if (grid[r][c] == '^') {
+                start_row = r;
+                start_col = c;
+            }
+        }
+    }
+
+    const part1: u64 = countVisisted(start_row, start_col, &obstacles);
     std.debug.print("{d}\n", .{part1});
-    // std.debug.print("{d}\n", .{part2});
+
+    var part2: u64 = 0;
+    for (0..GRID_SIZE) |r| {
+        for (0..GRID_SIZE) |c| {
+            if (obstacles[r][c] or (r == start_row and c == start_col)) {
+                continue;
+            }
+            obstacles[r][c] = true;
+            if (loops(start_row, start_col, &obstacles)) {
+                part2 += 1;
+            }
+            obstacles[r][c] = false;
+        }
+    }
+    std.debug.print("{d}\n", .{part2});
 }
