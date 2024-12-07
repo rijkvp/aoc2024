35d24d38e5c6b18ffcb7da24eca7355c70b0836a
Rijk van Putten <rijk@rijkvp.nl>
2024-12-07 11:09:40 +0100

    Day 7, part 2

--- day7/main.zig
+++ day7/main.zig
@@ -2,22 +2,34 @@ const std = @import("std");
 const input = @embedFile("input");
 const allocator = std.heap.page_allocator;
 
-fn canProduce2(current: u64, i: usize, target: u64, nums: []const u64) bool {
-    if (current == target) {
-        return true;
+fn concat(a: u64, b: u64) u64 {
+    var c = a;
+    var d = b;
+    while (d > 0) {
+        d /= 10;
+        c *= 10;
     }
-    if (i == nums.len) {
-        return false;
-    }
-    return canProduce2(current * nums[i], i + 1, target, nums) or canProduce2(current + nums[i], i + 1, target, nums);
+    return c + b;
 }
 
-fn canProduce(target: u64, nums: []const u64) bool {
-    return canProduce2(nums[0] * nums[1], 2, target, nums) or canProduce2(nums[0] + nums[1], 2, target, nums);
+fn canProduceInner(current: u64, i: usize, target: u64, nums: []const u64, comptime allowConcat: bool) bool {
+    if (i == nums.len) {
+        return current == target;
+    }
+    return canProduceInner(current * nums[i], i + 1, target, nums, allowConcat)
+        or canProduceInner(current + nums[i], i + 1, target, nums, allowConcat)
+        or (allowConcat and canProduceInner(concat(current, nums[i]), i + 1, target, nums, allowConcat));
+}
+
+fn canProduce(target: u64, nums: []const u64, comptime allowConcat: bool) bool {
+    return canProduceInner(nums[0] * nums[1], 2, target, nums, allowConcat)
+        or canProduceInner(nums[0] + nums[1], 2, target, nums, allowConcat)
+        or (allowConcat and canProduceInner(concat(nums[0], nums[1]), 2, target, nums, allowConcat));
 }
 
 pub fn main() !void {
     var part1: u64 = 0;
+    var part2: u64 = 0;
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
     while (lines.next()) |line| {
         var split = std.mem.tokenizeScalar(u8, line, ':');
@@ -26,13 +38,18 @@ pub fn main() !void {
         var nums = std.mem.tokenizeScalar(u8, split.next().?, ' ');
         var list = std.ArrayList(u64).init(allocator);
         defer list.deinit();
+
         while (nums.next()) |num| {
             try list.append(try std.fmt.parseInt(u64, num, 10));
         }
 
-        if (canProduce(testValue, list.items)) {
+        if (canProduce(testValue, list.items, false)) {
             part1 += testValue;
+        }
+        if (canProduce(testValue, list.items, true)) {
+            part2 += testValue;
         }
     }
     std.debug.print("{d}\n", .{part1});
+    std.debug.print("{d}\n", .{part2});
 }
