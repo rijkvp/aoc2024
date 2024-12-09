65af0d6f3ad78fdb6440082f81ea22de2f09e4c3
Rijk van Putten <rijk@rijkvp.nl>
2024-12-09 12:51:29 +0100

    Day 9, part 2

--- day9/main.zig
+++ day9/main.zig
@@ -2,27 +2,39 @@ const std = @import("std");
 const input = @embedFile("input");
 const alloc = std.heap.page_allocator;
 
-fn printDiskMap(diskMap: []?u64) void {
-    for (diskMap) |item| {
-        if (item) |i| {
-            std.debug.print("{d}", .{i});
-        } else {
-            std.debug.print(".", .{});
-        }
-    }
-    std.debug.print("\n", .{});
-}
-
 fn checksum(diskMap: []?u64) u64 {
     var sum: u64 = 0;
     for (diskMap, 0..) |value, i| {
-        sum += value.? * i;
+        if (value) |v| {
+            sum += v * i;
+        }
     }
     return sum;
 }
 
+fn findEmptyFragment(diskMap: []?u64, size: usize, limit: usize) ?[2]usize {
+    var i: usize = 0;
+    while (i < limit) {
+        if (diskMap[i] != null) {
+            i += 1;
+            continue;
+        }
+        var j = i + 1;
+        while (j < diskMap.len and diskMap[j] == null) {
+            j += 1;
+        }
+        if (j - i >= size) {
+            return .{ i, j };
+        }
+        i = j;
+    }
+    return null;
+}
+
 pub fn main() !void {
     var diskMap = std.ArrayList(?u64).init(alloc);
+    var sizes = std.ArrayList(u64).init(alloc);
+    var starts = std.ArrayList(u64).init(alloc);
     defer diskMap.deinit();
 
     var isFile: bool = true;
@@ -33,6 +45,8 @@ pub fn main() !void {
         }
         const size: u8 = char - '0';
         if (isFile) {
+            try sizes.append(size);
+            try starts.append(diskMap.items.len);
             try diskMap.appendNTimes(fileId, size);
             fileId += 1;
         } else {
@@ -40,6 +54,14 @@ pub fn main() !void {
         }
         isFile = !isFile;
     }
+
+    // copy diskmap for part 2
+    var diskMap2 = std.ArrayList(?u64).init(alloc);
+    defer diskMap2.deinit();
+    try diskMap2.resize(diskMap.items.len);
+    std.mem.copyForwards(?u64, diskMap2.items, diskMap.items);
+
+    // part one
     while (true) {
         const emptyIdx = std.mem.indexOfScalar(?u64, diskMap.items, null);
         if (emptyIdx) |idx| {
@@ -49,6 +71,20 @@ pub fn main() !void {
             break;
         }
     }
-
     std.debug.print("{d}\n", .{checksum(diskMap.items)});
+    // part two
+    for (1..fileId) |j| {
+        const id = fileId - j;
+        const fragmentSize = sizes.items[id];
+        const fragmentStart = starts.items[id];
+        if (findEmptyFragment(diskMap2.items, fragmentSize, fragmentStart)) |space| {
+            for (fragmentStart..(fragmentStart + fragmentSize)) |i| {
+                diskMap2.items[i] = null;
+            }
+            for (space[0]..(space[0] + fragmentSize)) |i| {
+                diskMap2.items[i] = id;
+            }
+        }
+    }
+    std.debug.print("{d}\n", .{checksum(diskMap2.items)});
 }
